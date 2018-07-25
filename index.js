0a7777214baef432e04a48b24679da8b038baee3
Emeka_darthvader <chybyke.emeka@gmail.com>
2018-07-25 11:01:13 +0100

    adding other congig

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
 
+const token = process.env.FB_VERIFY_TOKEN
+const access = process.env.FB_ACCESS_TOKEN
+
 app.set('port',(process.env.PORT || 4444))
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
@@ -13,13 +16,40 @@ app.get('/',function(req, res){
 })
 
 app.get('/webhook/',function(req, res){
-    if(req.query['hub.verify_token']=== 'my_voice_is_my_password_verify_me') {
+    if(req.query['hub.verify_token']=== token) {
         res.send(req.query['hub.challenge'])
     }
 
     res.send('No entry')
 })
 
+app.post('/webhook', (req, res) => {  
+
+    // Parse the request body from the POST
+    let body = req.body;
+  
+    // Check the webhook event is from a Page subscription
+    if (body.object === 'page') {
+  
+      // Iterate over each entry - there may be multiple if batched
+      body.entry.forEach(function(entry) {
+  
+        // Get the webhook event. entry.messaging is an array, but 
+        // will only ever contain one event, so we get index 0
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+        
+      });
+  
+      // Return a '200 OK' response to all events
+      res.status(200).send('EVENT_RECEIVED');
+  
+    } else {
+      // Return a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
+    }
+  
+  });
 
 app.listen(app.get('port'),function(){
 console.log('running on port', app.get('port'))
