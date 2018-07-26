2f1265599df71f2655ed0a1112e9075d0b2a28bf
Emeka_darthvader <chybyke.emeka@gmail.com>
2018-07-26 15:35:00 +0100

    uncommenting greeting and tryinng not to smmah

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.post('/webhook', (req, res) => {
   }
 
   function secondEntity(nlp, name){
-    return nlp && nlp.entities && nlp.entities[name] && nlp.entities[name][1];
+    return nlp && nlp.entities && nlp.entities[name] && nlp.entities[name][0];
   }
 
   function handleMessage(sender_psid, received_message) {
